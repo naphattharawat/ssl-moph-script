61baa71ef8bb6aaf04913221d225ec027deb6284
Naphattharawat S. <naphattharawat@gmail.com>
2024-09-23 11:48:15 +0700

    update

--- download_via_curl.sh
+++ download_via_curl.sh
@@ -3,6 +3,7 @@
 # กำหนด URL ของไฟล์ต่าง ๆ ที่อยู่ใน Git repository
 CONFIG_SCRIPT_URL="https://raw.githubusercontent.com/naphattharawat/ssl-moph-script/main/config.sh"
 INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/naphattharawat/ssl-moph-script/main/install.sh"
+UNINSTALL_SCRIPT_URL="https://raw.githubusercontent.com/naphattharawat/ssl-moph-script/main/uninstall.sh"
 SSL_SCRIPT_URL="https://raw.githubusercontent.com/naphattharawat/ssl-moph-script/main/ssl.sh"
 
 # สร้างไดเรกทอรีชั่วคราวสำหรับดาวน์โหลดไฟล์
@@ -11,6 +12,7 @@ TEMP_DIR=./ssl-script
 mkdir -p $TEMP_DIR
 # ดาวน์โหลดไฟล์ install.sh, config.sh และ ssl.sh ไปที่ไดเรกทอรีชั่วคราว
 curl -sS $INSTALL_SCRIPT_URL -o $TEMP_DIR/install.sh
+curl -sS $UNINSTALL_SCRIPT_URL -o $TEMP_DIR/uninstall.sh
 curl -sS $CONFIG_SCRIPT_URL -o $TEMP_DIR/config.sh
 curl -sS $SSL_SCRIPT_URL -o $TEMP_DIR/ssl.sh
 
