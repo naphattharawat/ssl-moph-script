6b6f0330807fdf4916bcf173ff8df2e693263566
Naphattharawat S. <naphattharawat@gmail.com>
2024-09-23 12:00:53 +0700

    update

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 version=1
 
 # Path ที่จะวางไฟล์ SSL เช่น /etc/nginx/ssl
-path=""
+path="/etc/nginx/ssl"
 
 # คำสั่งที่ใช้ในการ restart service เช่น systemctl restart nginx
 restart_command="systemctl restart nginx"
 
-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.f9gSOnolW0uUbQsD3G7XCEHxxSqn29Ao3b1V_k5jscA
+token=
